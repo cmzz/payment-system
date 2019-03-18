2eee87e4f9d8f3e30406a4780833a50c4eb0702b
zhuowu <zhuowu@hk01.com>
2019-03-18 14:15:12 +0800

    feat: add controllers

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +17,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// 下单
-Route::post('/order', 'OrderController@store');
+Route::group(['prefix' => 'v1'], function () {
+    // 同步回调
+    Route::any('/callback', 'CallbackController@index');
 
-// 支付订单
-Route::post('/order/{id}/pay', 'PayController@store')->where('id', '[0-9]+');
+    // 异步通知
+    Route::any('/notify', 'NofifyController@index');
+
+    // 下单
+    Route::post('/order', 'OrderController@store');
+
+    // 支付订单
+    Route::post('/order/{id}/pay', 'PayController@store')->where('id', '[0-9]+');
+});
