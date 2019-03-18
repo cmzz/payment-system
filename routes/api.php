061aaaa44360d601696da74990309ff61a6c459c
zhuowu <zhuowu@hk01.com>
2019-03-18 14:09:52 +0800

    feat: add controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// 下单
+Route::post('/order', 'OrderController@store');
+
+// 支付订单
+Route::post('/order/{id}/pay', 'PayController@store')->where('id', '[0-9]+');
