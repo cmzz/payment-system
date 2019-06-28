f1d2d3e02e355de420594e6a497d6207aa44847d
cmzz <me19@qq.com>
2019-06-28 21:09:42 +0800

    fix: 修复异步通知的问题

--- app/Payment/Order.php
+++ app/Payment/Order.php
@@ -94,12 +94,11 @@ class Order
 
                 $charge->refresh();
 
-                Log::channel('order')->info('支付成功, 订单状态更新成功', [
+                \Log::channel('order')->info('支付成功, 订单状态更新成功', [
                     'charge' => $charge
                 ]);
               
-//                event(new OrderPaidEvent($charge->{Charge::ID}));
-
+                event(new OrderPaidEvent($charge->{Charge::ID}));
             }
         });
     }
